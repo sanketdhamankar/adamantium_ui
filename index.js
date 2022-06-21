449c54d3c0778173c8f40b98ec94e29964c24c8e
sanketdhamankar <107908527+sanketdhamankar@users.noreply.github.com>
2022-06-21 11:30:23 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+import Button from './button';
+import ButtonGroup from './buttongroup';
+import Card from './card';
+import Checkbox from './checkbox';
+import Collapsible from './collapsible';
+import Column from './column';
+import Container from './container';
+import Datepicker from './datepicker';
+import DropdownMenu from './dropdownmenu';
+import Icon from './icon';
+import Input from './input';
+import Loading from './loading';
+import Modal from './modal';
+import Phonenumber from './phonenumber';
+import Progress from './progress';
+import RadioGroup from './radiogroup';
+import Select from './select';
+import Stepper from './stepper';
+import Switch from './switch';
+import Tag from './tag';
+import Text from './text';
+import ThemeProvider from './themeprovider';
+
+export {
+    Button,
+    ButtonGroup,
+    Card,
+    Checkbox,
+    Collapsible,
+    Column,
+    Container,
+    Datepicker,
+    DropdownMenu,
+    Icon,
+    Input,
+    Loading,
+    Modal,
+    Phonenumber,
+    Progress,
+    RadioGroup,
+    Select,
+    Stepper,
+    Switch,
+    Tag,
+    Text,
+    ThemeProvider,
+};
